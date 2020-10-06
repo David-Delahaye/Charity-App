6de388a57701a1f6f1f716a4fe9f46377210d142
David Delahaye <delahaye3@hotmail.co.uk>
2020-10-06 17:31:33 +0100

    add project feed to org page

--- src/components/pages/Organization.js
+++ src/components/pages/Organization.js
@@ -2,19 +2,23 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 import useCauses from "../utils/causes";
+import Feed from "../units/Feed";
 
 function Organization() {
   let { orgId } = useParams();
   console.log(orgId);
-  const [cause, loading] = useCauses("", orgId, "orgId");
-  if (cause !== null) console.log(cause[0]);
+  const [org, loading] = useCauses("", orgId, "orgId");
+  const [causes, loadingCauses] = useCauses("isha", undefined, "search");
+  if (org !== null) console.log(org);
 
   if (loading) return <h1>loading</h1>;
 
   return (
     <div>
-      <h1>{cause.name}</h1>
-      <p>{cause.mission}</p>
+      <h1>{org.name}</h1>
+      <p>{org.mission}</p>
+      <h1>Other projects</h1>
+      <Feed items={causes} loading={loadingCauses} />
     </div>
   );
 }
