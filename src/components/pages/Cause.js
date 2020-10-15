dbb77340cfe0995de6f9edbcc2fefd350737856b
David Delahaye <delahaye3@hotmail.co.uk>
2020-10-15 21:16:47 +0100

    add cause page content

--- src/components/pages/Cause.js
+++ src/components/pages/Cause.js
@@ -5,22 +5,33 @@ import useCauses from "../utils/causes";
 
 function Cause() {
   let { causeId } = useParams();
-  const [cause, loading] = useCauses("", causeId, "causeId");
+  let [cause, loading] = useCauses("", causeId, "causeId");
   if (cause !== null) console.log(cause[0]);
-
   if (loading) return <h1>loading</h1>;
 
+  cause = cause[0];
   return (
     <div>
       <div className="header">
-        <img src={cause[0].image.imagelink[4].url} />
+        <img src={cause.image.imagelink[4].url} />
         <h1>
-          {cause[0].title} <br />
-          by {cause[0].contactName}
+          {cause.title} <br />
+          by {cause.organization.name}
         </h1>
       </div>
       <div className="container">
-        <p>{cause[0].need}</p>
+        <h3>Summary</h3>
+        <p>{cause.summary}</p>
+        <h3>notice</h3>
+        <p>{cause.notice}</p>
+        <h3>need</h3>
+        <p>{cause.need}</p>
+        <h3>impact</h3>
+        <p>{cause.longTermImpact}</p>
+        <h3>Goal</h3>
+        <p>
+          {cause.funding} of {cause.goal} raised
+        </p>
       </div>
     </div>
   );
