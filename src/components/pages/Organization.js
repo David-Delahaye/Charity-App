3fac2543471127a2f4f63fcfe23356966124c6e4
David Delahaye <delahaye3@hotmail.co.uk>
2020-10-15 21:26:22 +0100

    add org page content

--- src/components/pages/Organization.js
+++ src/components/pages/Organization.js
@@ -11,11 +11,24 @@ function Organization() {
   const [causes, loadingCauses] = useCauses("", orgId, "causeByOrg");
   if (org !== null) console.log(org);
   if (loading) return <h1>loading</h1>;
+  console.log(org.themes);
 
   return (
     <div>
-      <h1>{org.name}</h1>
+      <h1>
+        {org.name}
+        <img src={org.logoUrl} />
+      </h1>
       <p>{org.mission}</p>
+      <div>
+        <ul>
+          <li>Country:{org.country}</li>
+          <li>City:{org.city}</li>
+          <li>Projects:{org.totalProjects}</li>
+          <li>Website:{org.url}</li>
+          <li>Themes:{org.themes.theme[0].name}</li>
+        </ul>
+      </div>
       <h1>Other projects</h1>
       <Slider items={causes} loading={loadingCauses} />
     </div>
